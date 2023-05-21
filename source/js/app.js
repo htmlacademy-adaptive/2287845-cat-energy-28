93f9d47b9dbf29620c5eb931b9d90cdc5882d9cf
Uluk <ulix.0500@gmail.com>
2023-05-21 10:39:09 +0300

    Js6

--- source/js/app.js
+++ source/js/app.js
@@ -2,6 +2,8 @@ let menuBtn = document.querySelector('.header__menu-button');
 let nav = document.querySelector('.nav')
 
 menuBtn.addEventListener('click', function (){
+  menuBtn.classList.remove('header__menu-button--active');
   menuBtn.classList.toggle('header__menu-button--active');
+  nav.classList.remove('nav--active');
   nav.classList.toggle('nav--active');
 })
