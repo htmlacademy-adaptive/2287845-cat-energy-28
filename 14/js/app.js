774b2b495ee94baf41899e533a864f6659b06a3b
Keks <keks@htmlacademy.ru>
2023-05-21 17:21:40 +0300

    :heavy_check_mark: Сборка #14

--- 14/js/app.js
+++ 14/js/app.js
@@ -1,9 +1,10 @@
 let menuBtn = document.querySelector('.header__menu-button');
 let nav = document.querySelector('.nav')
 
+menuBtn.classList.remove('header__menu-button--active');
+nav.classList.remove('nav--active');
+
 menuBtn.addEventListener('click', function (){
-  menuBtn.classList.remove('header__menu-button--active');
   menuBtn.classList.toggle('header__menu-button--active');
-  nav.classList.remove('nav--active');
   nav.classList.toggle('nav--active');
 })
